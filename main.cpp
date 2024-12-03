edc3524b5874e9660789a1703ea92b68502a14e3
MatheoGaillac <glc.matheo@gmail.com>
2024-12-03 14:59:34 +0100

    Fix

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ std::vector<Plat*> readCSVPlats(const std::string &nomFichier)
 int main()
 {
     //Création d'objet pour manipuler les différentes fonctionnalités du programme
-  Plat* plat1 = new Plat("Pizza Margherita", 17.0, {"tomates", "mozzarella", "basilic"}, true);
+/*  Plat* plat1 = new Plat("Pizza Margherita", 17.0, {"tomates", "mozzarella", "basilic"}, true);
     Plat* plat2 = new Plat("Spaghetti bolognaise", 15.0, {"pates", "sauce bolognaise", "parmesan"}, true);
 
     Client* client1 = new Client("Jean Dupuis", "06 11 22 33 44");
@@ -58,14 +58,14 @@ int main()
     std::vector<Plat*> platsCommandes1 = {plat1, plat2};
     commande1.setPlatsCommandes(platsCommandes1);
 
-    // Commande commande2(102, clientFidele1);
-    // commande2.addPlatsCommandes(plat2);
+    Commande commande2(102, clientFidele1);
+    commande2.addPlatsCommandes(plat2);
 
-    // std::cout << "=== Affichage de la commande 1 ===" << std::endl;
-    // commande1.afficherCommande();
+    std::cout << "=== Affichage de la commande 1 ===" << std::endl;
+    commande1.afficherCommande();
 
-    // std::cout << "\n=== Affichage de la commande 2 ===" << std::endl;
-    // commande2.afficherCommande();
+    std::cout << "\n=== Affichage de la commande 2 ===" << std::endl;
+    commande2.afficherCommande();
 
     std::cout << plat1->getNom() << std::endl;
     std::cout << plat2->getNom() << std::endl;
@@ -82,7 +82,7 @@ int main()
     delete plat1;
     delete plat2;
     delete client1;
-    delete clientFidele1;
+    delete clientFidele1; */
 
 
 
